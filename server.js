0e75fb8192e1b7fbf72438488c7e256c4cb1c973
dilleswarimuddada-crypto <dilleswarimuddada@gmail.com>
2026-04-06 18:02:08 +0000

    week 1 server running

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express= required ("express");
-const app = express();
-const port=3000;
-app.get("/",(req,res)=>{res.send("Enterprise-s
-app.sop agent server running")}
+const express=require("express");
+const multer =require("multer");
+const app=express();
+const PORT=3000;
+/*allow html file access*/
+app.use(express.static(__dirname));
+/*storage location*/
+const storage=multer.diskStorage({destination:function(req,file,cb){cb(null,"uploads/");},});
+const upload=multer({storage:storage});
+/*test route*/
+app.get("/",(req,res) => {res.send("Enterprise SOP Agent Server Running ");});
+/*upload route*/
+app.post("/upload", upload.single("file"),(req,res) => {res.send("File uploaded successfully");});
+app.listen(PORT,() => {console.log("server running on port 3000");});
